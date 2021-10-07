063cf0119cb56862502bc619c2fafec4c8e5000f
rsneal01 <rsneal01@gmail.com>
2021-10-07 09:23:03 -0700

    commit tree f79f832b81dfec6526249d7f1552af3cfcb7f631

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+
+function findMatching(drivers, name) {
+    let match = drivers.filter(x => x.toLowerCase().includes(name.toLowerCase()))
+
+    // let match = drivers.filter(n => {
+    //     n.toLowerCase();
+    //     return n === name;
+    // });
+    return match
+}
+
+function fuzzyMatch(drivers, letter) {
+    let match = drivers.filter(f => f.startsWith(letter))
+    return match
+}
+
+function matchName(drivers, searchTerm) {
+    let match = drivers.filter(f => f.name === searchTerm)
+    return match
+}
